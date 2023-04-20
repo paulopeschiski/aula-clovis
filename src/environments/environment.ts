2299cff94a1b78ea9eb838b3a37a82987595e926
paulopeschiski <paulohenriquepeschiski@gmail.com>
2023-04-20 16:41:01 -0300

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'kanbanfire-99b5e',
+    appId: '1:702177370324:web:b3d32d45e58d441adfdb41',
+    storageBucket: 'kanbanfire-99b5e.appspot.com',
+    apiKey: 'AIzaSyDNHyQc6VzGlS9ucY10udPc56WJNfxPpTg',
+    authDomain: 'kanbanfire-99b5e.firebaseapp.com',
+    messagingSenderId: '702177370324',
+    measurementId: 'G-YTBD5TDFJS',
+  },};
+
